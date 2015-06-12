e1e8d26f7cae5ead4082a3e73189664cf83b71cb
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-06-12 20:36:05 +0100

    Application: autoload models.

--- app.rb
+++ app.rb
@@ -4,3 +4,4 @@ class MyApp < Sinatra::Base
 end
 
 Dir[File.dirname(__FILE__) + '/controllers/*_controller.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
