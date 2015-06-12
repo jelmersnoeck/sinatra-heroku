a2f7c9c35cf68d4b500eb82dc0f15e52c71354fd
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-06-12 20:46:27 +0100

    Application: simplify configuration.

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
-require './config/application'
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'sinatra/activerecord'
 
 class MyApp < Sinatra::Base
+  def self.config_path
+    MyApp.root + '/config'
+  end
+
+  register Sinatra::ConfigFile
+  config_file "#{MyApp.config_path}/settings.yml"
+
+  register Sinatra::ActiveRecordExtension
+  set :database_file, "#{MyApp.config_path}/database.yml"
+
+  configure :development, :test do
+    require 'envyable'
+    Envyable.load("#{MyApp.config_path}/env.yml", MyApp.environment.to_s)
+  end
 end
 
 Dir[File.dirname(__FILE__) + '/app/controllers/*_controller.rb'].each { |file| require file }
