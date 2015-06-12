a832ffdb7fc5588a7c5370a87ad620cd414be461
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-06-12 20:38:25 +0100

    App: make app folder.
    More Rails like.

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ require './config/application'
 class MyApp < Sinatra::Base
 end
 
-Dir[File.dirname(__FILE__) + '/controllers/*_controller.rb'].each { |file| require file }
-Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/app/controllers/*_controller.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/app/models/*.rb'].each { |file| require file }
